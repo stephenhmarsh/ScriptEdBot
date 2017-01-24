d89e1b416a37e61a426c2849069ae4b5ea2d2e25
stephenhmarsh <stephenforpresident@gmail.com>
2017-01-24 10:20:20 -0500

    stubbing stuff out

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,6 @@
+class AttendancesController < ApplicationController
+
+  def new
+  end
+
+end
---
+++ app/models/attendance.rb
@@ -0,0 +1,11 @@
+class Attendance
+  # timestamps
+  # username via github
+  # ip address
+end
+
+
+
+
+# class has many students
+# class has many staff
