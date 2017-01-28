d330abb76ddf2c929f4cb7b102ac3f49190814cb
stephenhmarsh <stephenforpresident@gmail.com>
2017-01-27 20:29:58 -0500

    cleanup user code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
   devise :omniauthable, omniauth_providers: [:github]
+
+  has_many :attendances
 
   def self.new_with_session(params, session)
     super.tap do |user|
@@ -17,11 +17,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.name = auth.info.name
+      user.image = auth.info.image
     end
   end
 end
